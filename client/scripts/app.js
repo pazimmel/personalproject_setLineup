b16ac7fa5536bc2642c046c251519370fa851ffd
Paul Zimmel <pazimmel@gmail.com>
2015-11-13 15:40:38 -0600

    check lineup start

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -2,6 +2,14 @@ var myApp = angular.module("myApp", []);
 
 myApp.controller("LineupSet", ["$scope", "$http", function($scope, $http){
     $scope.teamArray = [];
+    $scope.starterfilter = true;
+    $scope.lineup = {
+        offense: 0,
+        defense: 0,
+        total: '$scope.lineup.offense+scope.lineup.defense'
+    };
+
+
     //$scope.teamLineup = {
     //    checked:['Sit']
     //};
@@ -10,7 +18,7 @@ myApp.controller("LineupSet", ["$scope", "$http", function($scope, $http){
         $http.get('/data').then(function(response){
             $scope.teamArray = response.data;
             //$scope.starters = $filter()
-            console.log($scope.teamArray);
+            //console.log($scope.teamArray);
         });
     };
 
@@ -19,5 +27,24 @@ myApp.controller("LineupSet", ["$scope", "$http", function($scope, $http){
         positionPlayer.starter = !positionPlayer.starter;
         //console.log(positionPlayer.starter);
     };
+    $scope.checkLineup = function(){
+
+      for(var i =0;i<$scope.teamArray.length; i++){
+
+          //console.log($scope.teamArray[i]);
+          $scope.player = $scope.teamArray[i];
+          //console.log($scope.player);
+
+          if($scope.player.starter==true){
+              console.log("Gotcha!");
+              $scope.lineup[$scope.player.position]++;
+          }
+          //check RB's
+          //check
+      }
+        console.log($scope.lineup);
+        return $scope.lineup;
+    };
+
     $scope.getPlayers();
 }]);
